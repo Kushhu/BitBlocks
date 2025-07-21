76606eeff636eee98cc63c7574aef16b4e7fbe61
Ramni Bhatt <kushagragangwal@gmail.com>
2025-07-21 08:30:22 +0530

    designed hmt prototype

---
+++ src/app/layout/minimal/minimal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minimal',
+  standalone: true,
+  imports: [],
+  templateUrl: './minimal.component.html',
+  styleUrl: './minimal.component.css'
+})
+export class MinimalComponent {
+
+}
